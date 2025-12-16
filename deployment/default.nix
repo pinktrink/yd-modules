3ea0abdae4a957aac190bdba82f27abb9dfb3e37
Princess Chloe <lolbummer@gmail.com>
2025-12-15 23:22:27 -0800

    Update deployment script.

--- deployment/default.nix
+++ deployment/default.nix
@@ -3,9 +3,49 @@
   inherit (pkgs) writeShellScript;
   inherit (lib) types;
   inherit (lib.meta) getExe;
+  inherit (lib.lists) filter genList elem;
   inherit (lib.options) mkOption;
+  inherit (lib.strings) concatStringsSep stringLength optionalString;
+  inherit (lib.attrsets) mapAttrsToList;
 
   cfg = config.physical.deployment;
+
+  getopts = opts: (
+    let
+      cases = mapAttrsToList
+        (n: v:
+          let
+            attrIsLong = stringLength n > 1;
+            short = "-${v.short or (optionalString (! attrIsLong) n)}";
+            long = "--${v.long or (optionalString attrIsLong n)}";
+            match = filter (x: !(elem x [ "-" "--" ])) [ short long ];
+            takes = v.takes or 1;
+            inherit (v) run;
+            shifts = concatStringsSep "\n" (genList (_: "shift") takes);
+          in
+          ''
+            ${concatStringsSep "|" match})
+              ${run}
+              ${shifts}
+            ;;
+          '')
+        opts;
+    in
+    ''
+      positionalOpts=()
+      while [[ "$#" -gt 0 ]]; do
+        currentOpt="$1"
+        shift
+        case $currentOpt in
+          ${concatStringsSep "\n" cases}
+
+          *)
+            positionalOpts+=("$currentOpt")
+          ;;
+        esac
+      done
+    ''
+  );
 in {
   options.physical.deployment = {
     configurationName = mkOption {
@@ -32,25 +72,50 @@ in {
         type = types.lines;
         default = let
           cfg' = cfg.ssh;
+          opts = {
+            i = {
+              long = "identity";
+              run = ''
+                identityFile="$1"
+              '';
+            };
+            h = {
+              long = "host";
+              run = ''
+                hostStr="$1"
+              '';
+            };
+            u = {
+              long = "user";
+              run = ''
+                userStr="$1"
+              '';
+            };
+            p = {
+              long = "port";
+              run = ''
+                portStr="$1"
+              '';
+            };
+            o = {
+              long = "sshopts";
+              run = ''
+                additionalSshOpts=" $1"
+              '';
+            };
+          };
         in (writeShellScript "deploy-${cfg.configurationName}" ''
-          case $1 in
-            s)
-              action=switch
-            ;;
-            t)
-              action=test
-            ;;
+          ${getopts opts}
+          host=''${hostStr:-${cfg'.address}}
+          user=''${userStr:-${cfg'.user}}
+          port=''${portStr:-${toString cfg'.port}}
+          action=''${positionalOpts[0]:-test}
+          case action in
             ssh)
               exec ${getExe pkgs.openssh} -p ${toString cfg'.port} ${cfg'.user}@${cfg'.address}
-            ;;
-            *)
-              action=''${1:-test}
-            ;;
+              ;;
           esac
-          echo -n "Host [${cfg'.address}]: "
-          read -r -t 5 host
-          host=''${host:-${cfg'.address}}:
-          NIX_SSHOPTS='-t -p ${toString cfg'.port}' ${getExe pkgs.nixos-rebuild} --flake .#${cfg.configurationName} --target-host ${cfg'.user}@${cfg'.address} --use-remote-sudo $action
+          NIX_SSHOPTS="''${additionalSshOpts:+$additionalSshOpts }-p $port''${identityFile:+ -i $identityFile}" ${getExe pkgs.nixos-rebuild} --flake .#${cfg.configurationName} --target-host $user@$host --use-remote-sudo $action
         '').outPath;
       };
       options = mkOption {
